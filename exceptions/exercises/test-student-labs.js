25ee42a85291cc5a018f58c417de06bc4de2f2a2
Matt Speaks <mattspeaks@hotmail.com>
2023-10-19 17:56:58 -0500

    Chapter 17 exercises

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,8 +1,13 @@
 function gradeLabs(labs) {
-  for (let i=0; i < labs.length; i++) {
-    let lab = labs[i];
-    let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
+  try {
+    for (let i=0; i < labs.length; i++) {
+      let lab = labs[i];
+      let result = lab.runLab(3);
+      console.log(`${lab.student} code worked: ${result === 27}`);
+    }
+  }
+  catch(err) {
+    console.log('Error Thrown');
   }
 }
 
@@ -21,4 +26,38 @@ let studentLabs = [
   }
 ];
 
-gradeLabs(studentLabs);
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
+
+//gradeLabs(studentLabs);
+
+
+
+//try {
+//   console.log('animal at index:', animals[index].name);
+// } catch(err) {
+//     console.log("We caught a TypeError, but our program continues to run!");
+//     console.log("You tried to access an animal at index:", index);
+// }
+
+
