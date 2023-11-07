ad08741e51f39811c08005c8d898d4b721f01711
Matt Speaks <mattspeaks@hotmail.com>
2023-11-06 20:09:12 -0600

    Fetch and JSON Studio

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,27 @@
 //TODO: Add Your Code Below
+window.addEventListener("load", ()=> {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(json){
+            console.log(json);
+            const bioInfo = document.getElementById("container");
+            for (let i=0; i < json.length; i++) {
+            bioInfo.innerHTML += `
+            <div class="astronaut">
+                <div class="bio">
+                    <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${json[i].hoursInSpace}</li>
+                        <li id="green${i}">Active: ${json[i].active}</li>
+                        <li>Skills: ${json[i].skills}</li>
+                    </ul>
+                </div>
+            <img class="avatar" src="${json[i].picture}">
+            </div>`
+            let greenText = document.getElementById(`green${i}`);
+            if (json[i].active){
+                greenText.style.color = "green";
+            }
+            };
+        })
+})
+})
