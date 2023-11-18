f5c98331984f6428ff25227514ad53b3c2340b49
Matt Speaks <mattspeaks@hotmail.com>
2023-11-18 02:29:44 -0600

    Exercises: Forms with some of Bonus Mission

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,27 @@
-//Code Your Solution Below
+    //Code Your Solution Below
+    window.addEventListener("load", function() {
+        let form = document.querySelector("form");
+        form.addEventListener("submit", function(event) {
+        let testName = document.querySelector("input[name=testName]");
+        let testDate = document.querySelector("input[name=testDate]");
+        let testValidDate = new Date(testDate);
+        let rocketType = document.querySelector("input[name=rocketType]");
+        let boosterCount = document.querySelector("input[name=boosterCount]");
+        let windRating = document.querySelector("input[name=windRating]");
+        let productionServers = document.querySelector("input[name=productionServers]");
+        let validateIfEmpty = [testName, testDate, rocketType, boosterCount, windRating, productionServers];
+        
+        // adding validation
+        
+        if (testName.value === "" || testDate.value === "") {
+                alert("All fields are required!");
+                event.preventDefault();
+            }
+            //testing date validation
+            if (!isNaN(testValidDate)) {
+                alert("All fields are required!");
+                event.preventDefault();
+            }
+        }); // end of event listener
+    });
+
---
+++ user-input-with-forms/exercises/test.js
@@ -0,0 +1,9 @@
+let date = "06/09/1988"
+
+let returnedValue = new Date(date);
+
+console.log(returnedValue);
+
+if (!isNaN(returnedValue)) {
+    console.log("True");
+}
